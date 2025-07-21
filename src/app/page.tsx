984fe7c5e532ecbf7317ac92c864022e6221afd6
mklebe <matthias.klebe@gmail.com>
2025-07-21 16:14:40 +0200

    nothing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,5 @@ export default function Home() {
   );
 }
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDj_zOb2e7ZrRYFIdb-kYRv_LdANyyaq20",
-  authDomain: "sommersonntage-tipps.firebaseapp.com",
-  projectId: "sommersonntage-tipps",
-  storageBucket: "sommersonntage-tipps.firebasestorage.app",
-  messagingSenderId: "686848289269",
-  appId: "1:686848289269:web:9c2832ce18f79bc07598bc"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
