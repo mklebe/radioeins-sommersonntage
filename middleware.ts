b5fa8082fef84514ff4178e7db65f75be1a27352
mklebe <matthias.klebe@gmail.com>
2025-07-27 20:32:14 +0200

    removes sonntage from home page

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// 1. Specify protected and public routes
-const protectedRoutes = [
-  '/sonntag',
-  '/sonntag/Top100Darkness',
-  '/sonntag/Top100Disko',
-  '/sonntag/Top100Fifties',
-  '/sonntag/Top100Friend',
-  '/sonntag/Top100Night',
-  '/sonntag/Top100NurEinWort',
-  '/sonntag/Top100OneLove',
-];
- 
+
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,12 @@
 
 import Link from "next/link";
-import { getSonntage } from "../services/database";
-import { useEffect, useState } from "react";
-import { Sonntag } from "../types";
+import { Typography } from "@mui/material";
 
 export default function Home() {
-  const [sonntage, setSonntage] = useState<Array<Sonntag>>([]);
-  useEffect(() => {
-    getSonntage().then((s) => setSonntage(s))
-  }, []);
-
   return (
     <div>
       <h1>Übersicht über alle Sonntage</h1>
-      <ul>
-        {sonntage
-        .sort((a, b) => a.date.getTime() - b.date.getTime())
-        .map(( {id, name, date} ) => {
-          return (
-            <Link key={id} href={`/sonntag/${id}`}>
-              <li style={{
-                padding: "8px",
-                listStyle: "none",
-              }}>
-              {name} - {id}<br />
-              {date.toLocaleString()}<br />
-              
-              </li>
-            </Link>)
-        })}
-      </ul>
+      <Typography>Du findest die aktuellen Sonntagslisten <Link href={`/sonntag/`}>auf der Kategorieseite</Link></Typography>
     </div>
   );
 };
