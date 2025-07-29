d18e91ab3697411c8e8b05841d6420ddd80e544b
mklebe <matthias.klebe@gmail.com>
2025-07-29 23:24:17 +0200

    add state messages to login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,24 +1,33 @@
+import { Typography } from "@mui/material";
 import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
 
 export default function Home() {
   const router = useRouter();
+  const [loginStateMessage, setLoginStateMessage] = useState<string>("");
+  const [userId, setUserId] = useState<string | null>(null);
 
-  const login = async (formData: FormData) => {
+  const login = async (evt: FormEvent) => {
+    evt.preventDefault();
+    setLoginStateMessage("Starte login");
     // Validate form fields
-    const userId = formData.get("username");
     const response = await fetch(`/api/user/${userId}`);
     if (response.status === 200) {
       router.push("/sonntag")
+      setLoginStateMessage("Login Erfolgreich");
+    } else {
+      setLoginStateMessage(`Konnte Nutzer ${userId} nicht anmelden. Bitte kontaktiere den Admin.`);
     }
   }
 
   return (
     <div>
       <h1>Hello stranger!</h1>
-      <form action={login}>
-        <label htmlFor="">Benutzer ID<input name="username" type="text"></input></label>
+      <form onSubmit={login}>
+        <label htmlFor="">Benutzer ID<input name="username" type="text" onChange={(evt) => setUserId(evt.target.value)}></input></label>
         <button type="submit">Anmelden</button>
       </form>
+      <Typography mt="24px" variant="body1">{loginStateMessage}</Typography>
     </div>
   );
 };
