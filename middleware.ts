a58d277fb5b974994c4b8faa9cb815fc20f15367
mklebe <matthias.klebe@gmail.com>
2025-07-29 22:38:34 +0200

    adds logging to middleware

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,11 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isProtectedRoute = path.includes("/sonntag/");
+  const isProtectedRoute = path.includes("/sonntag");
   const useridFromCookie = request.cookies.get("userid");
   
   if (isProtectedRoute) {
+    console.log("Middleware cookie value", useridFromCookie)
     if(!useridFromCookie || !useridFromCookie.value) {
       return NextResponse.redirect(new URL('/login', request.nextUrl))
     }
